929657d3337bee07f0bc57a7f6e2b71d8a7d01f5
xtr33me <wna_b_hacker@hotmail.com>
2017-06-26 14:57:25 -0400

    Checking in version that is taking advantage of batching in fit_generator, however I beleive it is slightly wrong

--- train.py
+++ train.py
@@ -816,3 +816,10 @@ for iteration in range(500):
 
 
 # In[ ]:
+model_json = model.to_json()
+with open("model/model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("model/model.h5")
+print("Saved model to disk")
+
